0620961450d34c781befd75ccd535e7c3e4cac5f
slo55 <slo55@my.bcit.ca>
2020-01-27 18:19:11 -0800

    added details for details.html

--- script2.js
+++ script2.js
@@ -7,21 +7,32 @@ const container = document.createElement("div");
 container.setAttribute("class","container");
 app.appendChild(container);
 
-window.onload = function(){
-    if(this.document.location.search=="?movieId=1"){
-        
-        fetch(url) 
-        .then(response => response.json())
-        .then (data =>{
-            //console.log(data);
-                data = data.results;
-                        console.log(data.opening_crawl);
+window.onload = function getDetails(){
+
+    fetch(url)    
+    .then(response => response.json())
+    .then(data=>{
+        data = data.results;
+
+        data.forEach(function(films){
+            //this.console.log(data.results);
+            if(document.location.search==`?movieId=${films.episode_id}`){
+                //console.log("hello2");
     
-                
-            })
-        .catch (function(error) {
-            console.log(error);
-        })
-    
-    }
+                var keys = Object.keys(films);
+
+                keys.forEach(function(key){
+                    var template = document.querySelector("#template");
+                    var clone = document.importNode(template.content, true);
+                    
+                    clone.querySelector(".h2").textContent = key;
+                    clone.querySelector(".p").textContent = films[key];
+
+                    app.appendChild(clone);
+
+                 }) 
+
+     
+        }})
+    })
 }
